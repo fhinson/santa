abfd2cad23355787f053ebe5c37579a557f1879c
Sabar Dasgupta <sabar@orchive.com>
2014-10-11 15:54:29 -0700

    here andaru

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,4 +1,4 @@
-class Driver < ActiveRecord::Base
+class Volunteer < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
