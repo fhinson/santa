367f22a8ee6ee7e9789c6e6e1061ae75d7a2b862
AndrewNg <andrew@chesscademy.com>
2014-10-11 15:36:03 -0700

    andrew sux

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'statics#home'
   resources :tags
 
+  resources
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
