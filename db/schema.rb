3d12b136702662f370be5d282343f5f9daf928e4
Francis Hinson <francis@orchive.com>
2014-10-11 14:29:15 -0700

    driver model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011200110) do
+ActiveRecord::Schema.define(version: 20141011212625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "drivers", force: true do |t|
+    t.string   "name"
+    t.string   "location"
+    t.string   "phone"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tags", force: true do |t|
     t.string  "name",                                null: false
