caccba718586a7e25d639c7ad8f93225b880bdcc
Liezl Puzon <liezl.200@gmail.com>
2014-10-11 13:53:05 -0700

    Different geolocation function

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,9 @@
 $(document).ready(getLocation);
 function getLocation(){
 	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(showPos);
+		navigator.geolocation.watchPosition(function(pos){
+			$("#loctext").append(pos.coords.latitude + "<br>" + pos.coords.longitude);
+		});
 	}
 	else { 
 		$("#loctext").append("Geolocation is not supported.");
