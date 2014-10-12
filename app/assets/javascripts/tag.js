5414564ad00529d3841df7c44b9c7bc3d44a0cb1
Sabar Dasgupta <sabar@orchive.com>
2014-10-12 13:07:11 -0700

    testing mobile

--- app/assets/javascripts/tag.js
+++ app/assets/javascripts/tag.js
@@ -1,11 +1,11 @@
 var accept = function () {
+  $("#lnkDialog").click(); 
   $.ajax({
     url: 'drivers/accept',
     type: 'PUT',
     data: {id: id},
     success: function () {
       $('#job').replaceWith("Accepted");
-      $("#lnkDialog").click(); 
     }
   });
 }
